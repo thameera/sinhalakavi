fcbdafed71362c3a2086aa3686b723076bd7dc4a
Thameera Senanayaka <thameera123@gmail.com>
2014-08-03 21:37:20 +0530

    Add gulp, jshint
    Signed-off-by: Thameera Senanayaka <thameera123@gmail.com>

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('lint', function() {
+  return gulp.src('./public/js/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('demon', function() {
+  nodemon({
+    script: 'web.js',
+    ext: 'js',
+    env: { 'NODE_ENV': 'development' }
+  })
+  .on('change', ['lint']);
+});
+
+gulp.task('default', ['lint', 'demon']);
+
